3ce2dcce3d5cc0bc2558d4c7ce71083900be6e7b
geraldofigueiredo <geraldo.figueiredosj@gmail.com>
2020-10-19 16:01:35 -0300

    feat: createRental checking movie availability

--- src/rental/rental.service.ts
+++ src/rental/rental.service.ts
@@ -1,26 +1,35 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Inject, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { plainToClass } from "class-transformer";
+import { ExecException } from "child_process";
+import { classToPlain, plainToClass } from "class-transformer";
 import { RentalDTO } from "src/dto/rental.dto";
+import { Movie } from "src/movie/movie.entity";
+import { MovieService } from "src/movie/movie.service";
 import { MovieRental } from "src/movieRental/movieRental.entity";
-import { Repository } from "typeorm";
+import { In, Repository } from "typeorm";
 import { Rental } from "./rental.entity";
 
 @Injectable()
 export class RentalService {
     constructor(
         @InjectRepository(Rental) private rentalRepository: Repository<Rental>,
-        @InjectRepository(MovieRental) private movieRentalRepository: Repository<MovieRental>
+        @Inject(MovieService) private movieService: MovieService
     ){}
 
-    async getRentals(): Promise<Rental[]> {
+    async findAll(): Promise<Rental[]> {
         return this.rentalRepository.find({relations: ['movies', 'movies.movie']});
+    }
+
+    async findOneById(rentalId: number): Promise<Rental> {
+        return this.rentalRepository.findOne(rentalId,{relations: ['movies', 'movies.movie']});
     }
 
     async createRental(rentalDTO: RentalDTO): Promise<Rental> {
         const rental = plainToClass(Rental, rentalDTO);
         rental.rentalDate = new Date();
+        await this.movieService.checkAvailability(rental.movies);
+        await this.movieService.rentMovies(rental.movies);
         await this.rentalRepository.save(rental);
-        return rental;
+        return this.findOneById(rental.id);
     }
 }
