379f0cd48c7b29afee1967171f7404fc5d77c325
geraldofigueiredo <geraldo.figueiredosj@gmail.com>
2020-10-19 15:52:14 -0300

    feat: set movieService as provider

--- src/rental/rental.module.ts
+++ src/rental/rental.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from 'src/movie/movie.entity';
+import { MovieService } from 'src/movie/movie.service';
 import { MovieRental } from 'src/movieRental/movieRental.entity';
 import { RentalController } from './rental.controller';
 import { Rental } from './rental.entity';
 import { RentalService } from "./rental.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Rental, MovieRental])],
+    imports: [TypeOrmModule.forFeature([Rental, MovieRental, Movie])],
     controllers: [RentalController],
-    providers: [RentalService],
+    providers: [RentalService, MovieService],
 })
 export class RentalModule {}
